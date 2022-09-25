56249754ea082e4bfca6ebb30e8fa9d48d937ab1
anepaz <anelisespaz@gmail.com>
2022-09-25 00:00:39 -0300

    first commit

---
+++ src/components/basics/ComParametro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ComParametro(props) {
+    const status = props.learningLevel > 1 ? 'Avançado' : 'Iniciante'
+    return (
+        <div>
+            <h2> { props.titulo } </h2>
+            <p> { props.learning } 
+            <span> e meu nível é: <strong> { status } </strong></span></p> 
+        </div>
+    )
+}
---
+++ src/components/basics/Primeiro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Primeiro() {
+    const msg = 'Full Stack Designer'
+    return (
+        <div>
+            <h2>Oi, eu sou a Ane Canabarro.</h2>
+            <h3><strong>{ msg }</strong></h3>
+        </div>
+    )  
+}
